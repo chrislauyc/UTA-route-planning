66ec54440faf04712af725aa7faf17ef46bdbb77
Chris Lau <chrislyc.lau@gmail.com>
2021-08-06 10:30:33 -0600

    index file and server file

---
+++ api/server.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const usersRouter = require("./users/users-router");
+const server = express();
+
+
+server.use(express.json());
+
+server.use("/api/users",usersRouter);
