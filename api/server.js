b393be6b671a1d3eaa3e8ed58d1b8d82f2571aa9
Chris Lau <chrislyc.lau@gmail.com>
2021-08-12 21:32:17 -0600

    removed cors require

--- api/server.js
+++ api/server.js
@@ -1,10 +1,10 @@
 const express = require("express");
 // const morgan = require("morgan");
-const cors = require("cors");
+// const cors = require("cors");
 const usersRouter = require("./users/users-router");
 const server = express();
-server.use(morgan("dev"));
-server.use(cors());
+// server.use(morgan("dev"));
+// server.use(cors());
 server.use(express.json());
 server.get("/",(req,res)=>{
     res.status(200).json({message:"server is responding"});
